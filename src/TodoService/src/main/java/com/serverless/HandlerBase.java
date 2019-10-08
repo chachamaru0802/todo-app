e90a1198acf4e24eedb84e02618ddad574c40439
chachamaru0802 <chachamaru.0802.k@gmail.com>
2019-10-09 01:20:21 +0900

    HandlerBase 新規作成

---
+++ src/TodoService/src/main/java/com/serverless/HandlerBase.java
@@ -0,0 +1,32 @@
+package com.serverless;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import org.apache.log4j.Logger;
+
+public abstract class HandlerBase<I, O> implements RequestHandler<I, O> {
+
+    protected abstract O process(I request, Context context);
+
+    protected void init() {
+
+    }
+
+    protected void destroy() {
+
+    }
+
+    @Override
+    public final O handleRequest(I i, Context context) {
+
+            init();
+
+            O result = process(i, context);
+
+            destroy();
+
+            return result;
+
+    }
+}
