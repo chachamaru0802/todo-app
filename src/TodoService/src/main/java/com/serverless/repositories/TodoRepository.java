45720d96a2c45fd14721562931da63dd72061dbc
chachamaru0802 <chachamaru.0802.k@gmail.com>
2019-10-09 01:37:30 +0900

    TodoRepository 新規作成

---
+++ src/TodoService/src/main/java/com/serverless/repositories/TodoRepository.java
@@ -0,0 +1,21 @@
+package com.serverless.repositories;
+
+import com.serverless.models.TodoItem;
+
+/**
+ * Todoリポジトリ
+ */
+public class TodoRepository {
+
+    /**
+     * Todo 取得
+     * @param id ID
+     * @return Todo情報
+     */
+    public TodoItem getTodoItem(String id){
+
+
+        return null;
+    }
+    
+}
