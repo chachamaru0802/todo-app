b47f3652e433ff454b9743896110416ea6c90998
chachamru0802 <chachamaru.0802.k@gmail.com>
2019-10-12 10:50:10 +0900

    GetTodolHandler Repositoryからデータを取得するように修正

--- src/TodoService/src/main/java/com/serverless/GetTodolHandler.java
+++ src/TodoService/src/main/java/com/serverless/GetTodolHandler.java
@@ -2,26 +2,38 @@ package com.serverless;
 
 import java.util.Map;
 
+import javax.inject.Inject;
+
 import com.amazonaws.services.lambda.runtime.Context;
-import com.serverless.models.TodoItem;
+import com.serverless.config.*;
+import com.serverless.config.AppComponent;
+import com.serverless.repositories.ITodoRepository;
 
 import org.apache.log4j.Logger;
 
 import lombok.val;
 
-
 public class GetTodolHandler extends HandlerBase<Map<String, Object>, ApiGatewayResponse> {
 
     private Logger _logger = Logger.getLogger(GetTodolHandler.class);
-   
+
+    
+    ITodoRepository _todoRepository;
+
+    @Inject
+    public GetTodolHandler() {
+        AppComponent appComponent = DaggerAppComponent.builder().build();
+        _todoRepository = appComponent.getTodoRepository();
+    }
+
     @Override
     protected ApiGatewayResponse process(Map<String, Object> request, Context context) {
 
-        val result = new TodoItem();
+        String id = "3";
 
-        result.setTitle("title");
-        result.setContents("Contents");
+        val result = _todoRepository.getTodoItem(id);
 
         return ApiGatewayResponse.responseBuild(result);
     }
+
 }
