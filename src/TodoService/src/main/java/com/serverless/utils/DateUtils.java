14d64e6d7ea50b578b15292f1a2866a70af3e055
chachamru0802 <chachamaru.0802.k@gmail.com>
2019-10-14 14:38:36 +0900

    日付ユーティリティ 実装

---
+++ src/TodoService/src/main/java/com/serverless/utils/DateUtils.java
@@ -0,0 +1,56 @@
+package com.serverless.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+import lombok.val;
+
+
+public final class DateUtils {
+    
+
+    private DateUtils(){}
+
+    public static Date toDate(long time){
+        val calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(time);
+        return calendar.getTime();
+    }
+
+    public static Date toDate(LocalDateTime datetime){
+        val zone = ZoneId.systemDefault();
+        val zonedDateTime = ZonedDateTime.of(datetime, zone);
+        val instant = zonedDateTime.toInstant();
+        return  Date.from(instant);
+    }
+
+    public static LocalDateTime toLocalDatetime(long time){
+        val date = toDate(time);
+
+        val result = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        return result;
+    }
+
+    public static long toTimeInMillis(Date value){
+        val calendar = Calendar.getInstance();
+        calendar.setTime(value);
+        return calendar.getTimeInMillis();
+    }
+
+    public static long toTimeInMillis(LocalDateTime value){
+        val date = toDate(value);
+       
+        return toTimeInMillis(date);
+    }
+
+    public static long toTimeInMillis(LocalDate value){
+        val date = Date.from(value.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        return toTimeInMillis(date);
+    }
+}
