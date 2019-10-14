6ba94eb6067b2aaca0401cb156e6ad1cc99b696e
chachamru0802 <chachamaru.0802.k@gmail.com>
2019-10-14 15:58:46 +0900

    ModelBaseにDyanmoDB定義を追加

--- src/TodoService/src/main/java/com/serverless/models/ModelBase.java
+++ src/TodoService/src/main/java/com/serverless/models/ModelBase.java
@@ -2,6 +2,8 @@ package com.serverless.models;
 
 import java.time.LocalDateTime;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIgnore;
 import com.serverless.utils.DateUtils;
 
 import lombok.Data;
@@ -10,21 +12,24 @@ import lombok.Data;
  * 基底モデルクラス
  */
 @Data
-public class ModelBase {
+public abstract class ModelBase {
 
     /**
      * 作成時間
      */
+    @DynamoDBAttribute(attributeName = "CreateTime")
     private long createTime;
 
     /**
      * 更新時間
      */
+    @DynamoDBAttribute(attributeName = "UpdateTime")
     private long updateTime;
 
     /**
      * 作成日時 取得
      */
+    @DynamoDBIgnore
     public LocalDateTime getCreateDatetime() {
         return DateUtils.toLocalDatetime(createTime);
     }
@@ -43,6 +48,7 @@ public class ModelBase {
      * 
      * @return
      */
+    @DynamoDBIgnore
     public LocalDateTime getUpdateDatetime() {
         return DateUtils.toLocalDatetime(updateTime);
     }
